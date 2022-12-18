712eb8e67fbb7e5c7e7eec9bf1a3b8ff8d71afe2
ARIF HOSSAIN <arifh6267@gmail.com>
2022-12-18 11:02:30 +0600

    Live-Shopping

--- src/components/layouts/FindStore.vue
+++ src/components/layouts/FindStore.vue
@@ -53,14 +53,6 @@
                         
                                 </div>
                             </div>
-                            <button class="carousel-control-prev" type="button" data-bs-target="#modal-product-image-inner" data-bs-slide="prev">
-                                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                                <span class="visually-hidden">Previous</span>
-                            </button>
-                            <button class="carousel-control-next" type="button" data-bs-target="#modal-product-image-inner" data-bs-slide="next">
-                                <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                                <span class="visually-hidden">Next</span>
-                            </button>
                         </div>
                     </div>
                     <div class="modal-product-details col-6 pt-3" v-if="details.length > 0">
